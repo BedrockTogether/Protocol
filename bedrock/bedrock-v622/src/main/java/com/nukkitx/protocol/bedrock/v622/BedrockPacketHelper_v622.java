0668bb1f3d5cfc8a2e26b2bfd73d51db7583ed00
Jedrzej <42713788+Extollite@users.noreply.github.com>
2023-10-23 12:20:50 +0200

    Support for 1.20.40

---
+++ bedrock/bedrock-v622/src/main/java/com/nukkitx/protocol/bedrock/v622/BedrockPacketHelper_v622.java
@@ -0,0 +1,11 @@
+package com.nukkitx.protocol.bedrock.v622;
+
+import com.nukkitx.protocol.bedrock.v589.BedrockPacketHelper_v618;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BedrockPacketHelper_v622 extends BedrockPacketHelper_v618 {
+
+    public static final BedrockPacketHelper_v622 INSTANCE = new BedrockPacketHelper_v622();
+}
---
+++ bedrock/bedrock-v622/src/main/java/com/nukkitx/protocol/bedrock/v622/serializer/DisconnectSerializer_v622.java
@@ -0,0 +1,30 @@
+package com.nukkitx.protocol.bedrock.v622.serializer;
+
+import com.nukkitx.network.VarInts;
+import com.nukkitx.protocol.bedrock.BedrockPacketHelper;
+import com.nukkitx.protocol.bedrock.BedrockSession;
+import com.nukkitx.protocol.bedrock.packet.DisconnectPacket;
+import com.nukkitx.protocol.bedrock.packet.DisconnectPacket.DisconnectFailReason;
+import com.nukkitx.protocol.bedrock.packet.ResourcePacksInfoPacket;
+import com.nukkitx.protocol.bedrock.v291.serializer.DisconnectSerializer_v291;
+import io.netty.buffer.ByteBuf;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DisconnectSerializer_v622 extends DisconnectSerializer_v291 {
+    public static final DisconnectSerializer_v622 INSTANCE = new DisconnectSerializer_v622();
+
+    @Override
+    public void serialize(ByteBuf buffer, BedrockPacketHelper helper, DisconnectPacket packet) {
+        VarInts.writeInt(buffer, packet.getReason().ordinal());
+        super.serialize(buffer, helper, packet);
+    }
+
+    @Override
+    public void deserialize(ByteBuf buffer, BedrockPacketHelper helper, DisconnectPacket packet) {
+        packet.setReason(DisconnectFailReason.values()[VarInts.readInt(buffer)]);
+        super.deserialize(buffer, helper, packet);
+    }
+}
+
